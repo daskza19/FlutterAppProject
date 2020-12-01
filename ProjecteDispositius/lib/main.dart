f39a00048d5bb2ea48eea049aa443ebfddc03791
Arnau77 <47742576+Arnau77@users.noreply.github.com>
2020-12-01 10:57:16 +0100

    main screen now shows everything correctly

--- ProjecteDispositius/lib/main.dart
+++ ProjecteDispositius/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:omdb_dart/omdb_dart.dart';
 
 import 'screens/itemslist.dart';
 
--- ProjecteDispositius/lib/widgets/mainListWidget.dart
+++ ProjecteDispositius/lib/widgets/mainListWidget.dart
@@ -11,62 +11,65 @@ class MainListWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100,
       decoration: BoxDecoration(
         color: Colors.black.withOpacity(0.6),
         borderRadius: BorderRadius.all(Radius.circular(10)),
       ),
       padding: EdgeInsets.all(8),
       child: Row(
+        mainAxisSize: MainAxisSize.max,
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           ClipRRect(
             borderRadius: BorderRadius.circular(10),
-            child: Image.network(item.posterURL),
+            child: Image.network(item.posterURL,height: 100,),
           ),
           SizedBox(
             width: 6,
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                item.mediaName,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
+          Expanded(
+                      child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  item.mediaName,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+
                 ),
-              ),
-              Text(
-                'Any: ' + item.year,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 10,
+                Text(
+                  'Any: ' + item.year,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 10,
+                  ),
                 ),
-              ),
-              Text(
-                'Duració: ' + item.duration,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 10,
+                Text(
+                  'Duració: ' + item.duration,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 10,
+                  ),
                 ),
-              ),
-              Text(
-                'Génere: ' + item.genres,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 10,
+                Text(
+                  'Génere: ' + item.genres,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 10,
+                  ),
                 ),
-              ),
-              Text(
-                'Valoració: ' + item.valoration,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 10,
+                Text(
+                  'Valoració: ' + item.valoration,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 10,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
