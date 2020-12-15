3d860129eb24af5a682109393fac77f23b134534
Arnau77 <47742576+Arnau77@users.noreply.github.com>
2020-12-15 08:47:28 +0100

    when longpressing an item, this is deleted

--- ProjecteDispositius/lib/widgets/mainListWidget.dart
+++ ProjecteDispositius/lib/widgets/mainListWidget.dart
@@ -1,4 +1,5 @@
 import 'package:ProjecteDispositius/item.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class MainListWidget extends StatelessWidget {
@@ -10,68 +11,86 @@ class MainListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.black.withOpacity(0.6),
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-      ),
-      padding: EdgeInsets.all(8),
-      child: Row(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Image.network(item.posterURL,height: 100,),
-          ),
-          SizedBox(
-            width: 6,
-          ),
-          Expanded(
-                      child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  item.mediaName,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-
-                ),
-                Text(
-                  'Any: ' + item.year,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 10,
-                  ),
-                ),
-                Text(
-                  'Duració: ' + item.duration,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 10,
-                  ),
-                ),
-                Text(
-                  'Génere: ' + item.genres,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 10,
-                  ),
-                ),
-                Text(
-                  'Valoració: ' + item.valoration,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 10,
-                  ),
-                ),
-              ],
+  final movies = FirebaseFirestore.instance.collection('ListToView');
+    return FlatButton(
+      onPressed: () {},
+      onLongPress: () {
+         movies.doc(item.id).delete();
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text('Item "${item.mediaName}" deleted'),
+                      action: SnackBarAction(
+                        label: 'UNDO',
+                        onPressed: () {
+                          movies.add(item.toFirestore());
+                        },
+                      ),
+                    ));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.6),
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        padding: EdgeInsets.all(8),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(
+                item.posterURL,
+                height: 100,
+              ),
             ),
-          ),
-        ],
+            SizedBox(
+              width: 6,
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    item.mediaName,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                  Text(
+                    'Any: ' + item.year,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 10,
+                    ),
+                  ),
+                  Text(
+                    'Duració: ' + item.duration,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 10,
+                    ),
+                  ),
+                  Text(
+                    'Génere: ' + item.genres,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 10,
+                    ),
+                  ),
+                  Text(
+                    'Valoració: ' + item.valoration,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 10,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
